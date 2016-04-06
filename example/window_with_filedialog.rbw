7aad34707d0339cb181a65cc31f0f45fc4b7679f
sxysxy <1441157749@qq.com>
2016-04-06 14:04:03 +0800

    完善文件对话框功能

--- example/window_with_filedialog.rbw
+++ example/window_with_filedialog.rbw
@@ -1,10 +1,19 @@
 require 'XYGuiCore'
 
 XYApp.new("fff") do |app|
-	XYMainWindow.new(app, nil, {:title => 'file dialog test'}) do |wnd|
-		XYPushButton.new(app, wnd, {:text => "Show File Dialog", :width => 200}).set do
+	XYMainWindow.new(app, nil, {:title => 'file dialog test', :layout => XYVerticalLayout}) do |wnd|
+		XYPushButton.new(app, wnd, {:text => "Show Open File Dialog"}).set do
 			connect(:ON_COMMAND) do |sender, data|
-				XYFileDialog.show
+				f = XYFileDialog.show(wnd, {:multiple => true, :title => "Open a file to play", :okcreate => true, 
+						:filter => "All Files(*.*)\0*.*\0Ruby File(*.rb)\0*.rb\0RubyW File(*.rbw)\0*.rbw\0Text File(*.txt)\0*.txt\0\0"}) #note, this format will be replaced soon
+				XYMessageBox.show("Tip", "You have chosen #{f}")
+			end
+		end
+		XYPushButton.new(app, wnd, {:text => "Show Open Save Dialog"}).set do
+			connect(:ON_COMMAND) do |sender, data|
+				f = XYFileDialog.show(wnd, {:save => true, :title => "Save a file to play",
+					:filter => "All Files(*.*)\0*.*\0Ruby File(*.rb)\0*.rb\0RubyW File(*.rbw)\0*.rbw\0Text File(*.txt)\0*.txt\0\0"})
+				XYMessageBox.show("Tip", "You have chosen #{f}")
 			end
 		end
 	end.show
--- lib/XYGui/xy_filedialog.rb
+++ lib/XYGui/xy_filedialog.rb
@@ -10,24 +10,50 @@ class XYFileDialog < XYWidget
 	OFN_PATHMUSTEXIST = 0x800
 	OFN_FILEMUSTEXIST = 0x1000
 	OFN_EXPLORER = 0x80000
-	
+	OFN_ALLOWMULTISELECT = 0x200
+	OFN_CREATEPROMPT = 0x2000
+
 	def initialize
 		
 	end
 	
-	def self.show
-		@buf = "\x00"*256
-		pack = [76, 0, 0,  			    #size, owner, instance
-				"*.*\x00", 0,   			#fitter, customFilter
-				0, 0, 		 			#maxCustFilter, filterIndex
-				@buf, 64, 0, 0,   			#file, maxFile, fileTitle, maxFileTitle
-				Dir.pwd.gsub("/", "\\"),					#PATH
-				"Choose File",
-				OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST,   #flag
-				0,						#fileOffset 		! 2byte
-				0,						#fileExtension      ! 2byte
-				0, 0, 0, 0              #defExt, custData, hookProc, templateNmae
+	def self.show(parent = nil, arg = {})
+		@owner = (parent.is_a?(XYWidget))? parent.handle: 0
+		@filter = arg[:filter]? arg[:filter]: "*.*\x00"
+		@flag = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST
+		#@flag |= OFN_ALLOWMULTISELECT if arg[:multiple]       #reserve 
+		@flag |= OFN_CREATEPROMPT if arg[:okcreate]
+		
+		@save = arg[:save]? arg[:save]: false
+		@flag &= (~OFN_ALLOWMULTISELECT) if @save
+		@title = arg[:title]
+		if !@title
+			if @save
+				@title = "Save File"
+			else
+				@title = "Open File"
+			end
+		end
+
+		@buf = "\x00"*256	#must be '\x00'
+		pack = [76, @owner, 0,  			#size, owner, instance
+				@filter, 0,   				#fitter, customFilter
+				0, 0, 		 				#maxCustFilter, filterIndex
+				@buf, 256, 0, 0,   			#file(buffer), maxFile(buffer size), fileTitle, maxFileTitle
+				Dir.pwd.gsub("/", "\\"),	#PATH
+				@title,						#title
+				@flag, 					 	#flag
+				0,							#fileOffset 		! 2byte
+				0,							#fileExtension      ! 2byte
+				0, 0, 0, 0             		#defExt, custData, hookProc, templateNmae
 				].pack("LLLpLLLpLLLppLSSLLLL")
-		WinAPI.call("comdlg32", "GetOpenFileName", pack)
+
+		if(!@save)
+			ret = WinAPI.call("comdlg32", "GetOpenFileName", pack)
+			return @buf 
+		else
+			ret = WinAPI.call("comdlg32", "GetSaveFileName", pack)
+			return @buf 
+		end
 	end
 end
